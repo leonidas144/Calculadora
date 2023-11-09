062ed5471369f35ea4f5d8210abf4e7548afdede
Leonidas <leo_alvarenga92@hotmail.com>
2023-11-09 12:56:49 -0300

    Documentação

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,23 +1,52 @@
 package calculadora;
 
+/**
+ * @author Leônidas Alvarenga
+ * @version 1.03
+ * @since Release 1.01 da aplicação
+ * 
+ * Classe que realiza operações matemáticas básicas como adição, subtração,
+ * multiplicação e divisão.
+ */
+
 public class Calculadora {
 	
-	//Método de soma
+	/**
+	 * Realiza operação de adição.
+	 * @param a O primeiro elemento da operação.
+	 * @param b O segundo elemento da operação.
+	 * @return Retorna o resultado da operação.
+	 */
 	public int soma (int a, int b) {
 		return a + b;
 	}
 	
-	//Método de subtração
+	/**
+	 * Realiza operação de subtração.
+	 * @param a O primeiro elemento da operação.
+	 * @param b O segundo elemento da operação.
+	 * @return Retorna o resultado da operação.
+	 */
 	public int sub (int a, int b) {
 		return a - b;
 	}
 	
-	//Método de multiplicação
+	/**
+	 * Realiza operação de multiplicação.
+	 * @param a O primeiro elemento da operação.
+	 * @param b O segundo elemento da operação.
+	 * @return Retorna o resultado da operação.
+	 */
 	public int mult (int a, int b) {
 		return a * b;
 	}
 	
-	//Método de divisão
+	/**
+	 * Realiza operação de divisão.
+	 * @param a O primeiro elemento da operação.
+	 * @param b O segundo elemento da operação.
+	 * @return Retorna o resultado da operação.
+	 */
 	public int div (int a, int b) {
 		return a / b;
 	}
